adab55a0beca79dd172c4e3de9fc4280e363598b
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2021-04-26 10:29:32 -0300

    Finalização NLW-dev_quiz

--- dev_quiz/lib/core/app_widget.dart
+++ dev_quiz/lib/core/app_widget.dart
@@ -1,14 +1,13 @@
-import 'package:DevQuiz/challenge_page/challenge_page.dart';
-import 'package:DevQuiz/home/home_page.dart';
-import 'package:DevQuiz/splah/splash_page.dart';
+import 'package:dev_quiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "DevQuiz",
-      home: ChallengePage(),
+      home: SplashPage(),
     );
   }
 }
